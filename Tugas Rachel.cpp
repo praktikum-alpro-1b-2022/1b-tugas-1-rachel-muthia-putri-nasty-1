688297bb4ac44df9b2e55e117452c32d78714f3c
Rachel Muthia <114643400+RachelMuthia18@users.noreply.github.com>
2022-11-13 20:06:59 +0700

    Add files via upload

---
+++ Tugas Rachel.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+    //Tugasnya NPM, NAMA LENGKAP, PRODI
+int main (){
+    cout << "NPM : 2210631170097\n";
+    cout << "Nama lengkap : Rachel Muthia Putri Nasty" << endl;
+    cout << "Umur : 18" << endl;
+    cout << "Kelas : 1B" << endl;
+    cout << "Hobby : Dengar musik, nari, berenang" << endl;
+    cout << "Cita cita : Ketua DPR" << endl;
+
+    return 0;
+
+}
