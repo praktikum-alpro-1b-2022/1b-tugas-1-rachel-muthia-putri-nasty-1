b42c1b9662d9f4a86571bb7fbba09a4cac633b69
Rachel Muthia <114643400+RachelMuthia18@users.noreply.github.com>
2022-11-13 19:58:41 +0700

    Add files via upload

---
+++ praktikum alpro 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main () {
+
+
+    int umur =19;
+    cout << "umur saya adalah" << endl ;
+    umur = 25;
+
+    cout << "umur saya adalah " <<umur;
+    return 0;
+}
